f4109448bcbf420df4f30b51cf6cd0368ca7710e
Chua, Huiyuan <weiyuan.cai@gmail.com>
2024-03-14 19:05:06 +0800

    save work on data

--- mayo.py
+++ mayo.py
@@ -10,7 +10,6 @@ def unzip(zip_path):
     with zipfile.ZipFile(zip_path) as zf:
         files = []
         for file_name in zf.namelist():
-            print(file_name)
             if not file_name.endswith('.dcm'):
                 continue
 
@@ -22,20 +21,25 @@ def unzip(zip_path):
     return files
 
 def get_training_data(folder):
-    ds = []
-    pathlist = Path(folder).rglob('DICOM-CT-PD_QD.zip')
+    fd_ima = []
+    qd_ima = []
+    fd_path=f'{folder}/full_3mm'
+    qd_path=f'{folder}/quarter_3mm'
+    pathlist = Path(folder).rglob('*.ima')
     for zip_path in pathlist:
-        files_in_zip = unzip(zip_path)
-        for file_name, file_string in files_in_zip:
-            f = dcmread(fp=filepath)
-            ds.append(f.pixel_array)
-    return np.stack(ds)
+        zip_file_name = os.path.basename(zip_path)
+        dcm_files = unzip(zip_path)
+        for file_name, dcm in dcm_files:
+            ds.append((zip_file_name, file_name, dcm))
+    return ds
 
 
 def main():
-  #path='/Volumes/SEAGATE_1TB/Huiyuan/projects/Mayo_Grand_Challenge/Patient_Data/Training_Projection_Data/L067/DICOM-CT-PD_QD.zip'
-    path='/media/huiyuanchua/SEAGATE_1TB/Huiyuan/projects/Mayo_Grand_Challenge/Patient_Data/Training_Projection_Data/L067/DICOM-CT-PD_QD.zip'
-  print(unzip(path))
+    #path='/Volumes/SEAGATE_1TB/Huiyuan/projects/Mayo_Grand_Challenge/Patient_Data/Training_Projection_Data/L067/DICOM-CT-PD_QD.zip'
+    path='/Users/huiyuanchua/Documents/Mayo_Grand_Challenge/Patient_Data/Training_Image_Data/3mm B30'
+    #path='/media/huiyuanchua/SEAGATE_1TB/Huiyuan/projects/Mayo_Grand_Challenge/Patient_Data/Training_Projection_Data/L067/DICOM-CT-PD_QD.zip'
+    training_data = get_training_data(path)
+    print(len(training_data))
 
 if __name__ == '__main__':
     main()
